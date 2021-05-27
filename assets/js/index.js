17c878999671e0a60b452c703ed37c0a88fa28ea
Vitor <milagre.vitor@gmail.com>
2021-05-27 20:40:02 +0100

    Addition of hamburger menu

--- assets/js/index.js
+++ assets/js/index.js
@@ -26,3 +26,24 @@ botao.addEventListener('click', function(){
     const codigo = areaDoCodigo.querySelector('code');
     hljs.highlightBlock(codigo);
 });
+
+/*menu-hamburguer*/
+const botaoBarras = document.querySelector('.menu-responsivo');//
+const botaoXis = document.querySelector('.fechar-menu');
+const menu = document.querySelector('.menu');
+
+botaoBarras.addEventListener('click', function(){
+    menu.classList.add('menu-open');
+    menu.classList.remove('menu');
+    botaoXis.classList.add('xis');
+    botaoBarras.classList.add('naoBarras');
+    botaoBarras.classList.remove('menu-responsivo');
+});
+
+botaoXis.addEventListener('click', function(){
+    menu.classList.remove('menu-open');
+    menu.classList.add('menu');
+    botaoXis.classList.remove('xis');
+    botaoBarras.classList.remove('naoBarras');
+    botaoBarras.classList.add('menu-responsivo');
+});
