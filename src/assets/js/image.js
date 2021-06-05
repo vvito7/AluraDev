f6ce6a1d3beb01d833734e483a24112eac4d2c0b
Vitor <milagre.vitor@gmail.com>
2021-06-05 20:13:44 +0100

    Fixed image bug

--- src/assets/js/image.js
+++ src/assets/js/image.js
@@ -1,10 +1,10 @@
 import domtoimage from 'dom-to-image';
 import { saveAs } from 'file-saver';
-const quadro = document.querySelector('#editor-codigo');
+const quadro = document.querySelector('#quadro');
 const botao = document.querySelector('#botao-exportar');
 
 botao.addEventListener('click', function(){
-    domtoimage.toBlob(quadro)
+    domtoimage.toPng(quadro)
     .then(function (blob) {
         window.saveAs(blob, 'code.png');
     });
